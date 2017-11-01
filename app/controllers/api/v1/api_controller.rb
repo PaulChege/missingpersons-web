6ddd921a02dc96e04f65d17735b1dde2d0cc9430
PaulChege <pchegenjenga@gmail.com>
2017-11-01 12:52:48 +0300

    changes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::ApiController < Api::V1::BaseController
   def create_case
-      c = PublicUser.find(params[:public_user_id]).cases.new(name: params[:name])
+      c = PublicUser.find(params[:public_user_id]).cases.new(
+          name: params[:name],
+          age: params[:age],
+          height: params[:height],
+          weight: params[:weight],
+          body_type: params[:body_type],
+          date_last_seen: params[:date_last_seen],
+          location_last_seen: params[:location_last_seen],
+          description: params[:description],
+          status: "Active",
+          date_reported: Time.now,
+          reporter_rel: params[:reporter_rel],
+          town: params[:town]
+      )
       if c.save!
         render json: {status: "success"}
     else
